e8df9753a41e38ef3928e21af10afb870858fd7a
Chandler Bath & Kitchen <dev@chandlerbathandkitchen.com>
2026-03-10 22:15:52 +0500

    Fix syntax error in service-faq component

--- components/services/service-faq.tsx
+++ components/services/service-faq.tsx
@@ -60,7 +60,7 @@ export default function ServiceFAQ({ faqs }: ServiceFAQProps) {
                     {faq.answer}
                   </AccordionContent>
                 </AccordionItem>
-              ))
+              ))}
             </Accordion>
           </motion.div>
         </div>
